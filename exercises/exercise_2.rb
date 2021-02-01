13d3a76a2b190a3e3c4462c8c028ff8f28d50d31
Joseph Williams <josephwilliamsjs@gmail.com>
2021-02-01 21:34:34 +0000

    finishing exercise 4

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -6,5 +6,5 @@ puts "----------"
 
 # Your code goes here ...
 @store1 = Store.find_by(id: 1)
-@store2 = Store.second
-@store1.update(name: 'Surrey')
+@store2 = Store.find_by(id: 2)
+@store1.update(name: 'New West')
