780eebe910ca6b862b32475fbc6156f98b6e53d6
Joseph Williams <josephwilliamsjs@gmail.com>
2021-02-01 22:30:14 +0000

    finishing the exercises

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Joe", last_name: "Williams", hourly_rate: 65)
+@store1.employees.create(first_name: "Brian", last_name: "Jones", hourly_rate: 70)
+@store1.employees.create(first_name: "Harry", last_name: "Potter", hourly_rate: 70)
+
+@store2.employees.create(first_name: "Alice", last_name: "Cooper", hourly_rate: 50)
+@store2.employees.create(first_name: "Bob", last_name: "Ross", hourly_rate: 55)
+@store2.employees.create(first_name: "Peter", last_name: "Parker", hourly_rate: 60)
+
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,15 @@
 class Store < ActiveRecord::Base
+  has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { 
+    only_integer: true, 
+    greater_than: 0
+  }
+  validate :must_carry_apparel
 
+  def must_carry_apparel
+    if mens_apparel.blank? && womens_apparel.blank?
+      errors.add(:base, message: "Store must sell either men or womens apparel")
+    end
+  end 
 end
