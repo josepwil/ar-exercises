ae43c1ca9b871260afb361ceb48392f29fefebb3
Joseph Williams <josephwilliamsjs@gmail.com>
2021-02-01 23:20:34 +0000

    finishing exercise 9

--- exercises/exercise_8.rb
+++ exercises/exercise_8.rb
@@ -7,7 +7,7 @@ require_relative './exercise_5'
 require_relative './exercise_6'
 require_relative './exercise_7'
 
-puts "Exercise 7"
+puts "Exercise 8"
 puts "----------"
 
 # Your code goes here ...
--- lib/store.rb
+++ lib/store.rb
@@ -6,10 +6,19 @@ class Store < ActiveRecord::Base
     greater_than: 0
   }
   validate :must_carry_apparel
+  before_destroy :check_employees, prepend: true
 
   def must_carry_apparel
     if mens_apparel.blank? && womens_apparel.blank?
       errors.add(:base, message: "Store must sell either men or womens apparel")
     end
   end 
+
+  private
+  def check_employees
+    @store = Store.find(self.id) 
+    if @store.employees.size >= 1
+      false;
+    end
+  end
 end
